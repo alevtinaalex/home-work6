9b89f709b788643e39174123c8773371021aba4f
Alevtina <_@email.comgit>
2022-09-20 19:41:02 +0300

    Add vveli M chisel i skolko bolshe 0

---
+++ chiselM_bolshe0/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.Write("Сколько чисел нужно ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+EnterNumbers(numbers);
+MoreZero(numbers);
+
+void EnterNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write("Введите {0} число ", i + 1);
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void MoreZero(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+            count = count + 1;
+    }
+    System.Console.WriteLine($"Количество чисел больше нуля = {count}");
+}
